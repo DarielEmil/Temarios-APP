97856a46f824df65745205be0657449bc8c91ced
Cristian Eulises Sanchez Ramirez <scristianeulises@gmail.com>
2023-11-22 20:22:11 -0400

    Database config

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const connection = require("./context/connection");
+const dotenv = require("dotenv");
+
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json());
-
 
 app.get("/", (req, res) => {
    res.json({ working: "THE APP IS WORKING" });
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
    console.log("server started on port 5000");
 });
 
