651b187f43c0a875b2357f1723f033c05d6dc2b2
FrankR27 <frankdejesusramosguzman9@gmail.com>
2023-11-22 21:46:25 -0400

    Login backend

---
+++ server/controllers/authController.js
@@ -0,0 +1,36 @@
+const User = require('../models/User');
+
+const iniciarSesion = async (req, res) => {
+    const { email, password } = req.body;
+    console.log(req.body);
+
+    try {
+        const usuario = await User.findOne({ email });
+
+        if(!usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Revisa los datos ingresados'
+            });
+        }
+
+        // Confirmar los passwords
+        if(usuario.password !== password) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Revisa los datos ingresados',
+            });
+        }
+
+        // Redireccionar al usuario a la ruta principal, pero mientras tanto mostramos "conectado" en consola
+        console.log('Conectado');
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hubo un error al iniciar sesión', error
+        });
+    }
+}
+
+module.exports = { iniciarSesion }
