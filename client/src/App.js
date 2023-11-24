4b9bf1c5fac272fb0448c30653d77d1c574c675e
Cristian Eulises Sanchez Ramirez <scristianeulises@gmail.com>
2023-11-23 22:07:29 -0400

    some fix

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import { Route } from "react-router-dom/cjs/react-router-dom";
+import "./App.css";
+import Register from "./components/Register/Register";
+import { BrowserRouter as Router, Switch } from "react-router-dom/cjs/react-router-dom.min";
+
+function App() {
+   return (
+      <Router>
+         <div className="App">
+            <Switch>
+               <Route path="/signup">
+                  <Register />
+               </Route>
+            </Switch>
+         </div>
+      </Router>
+
+   );
+
+}
+
+export default App;
