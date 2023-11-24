e05845e77734d4d105a37d8b3cdeb96365a02c91
Restituy0 <dariel_eduardo@hotmail.com>
2023-11-24 15:23:30 -0400

    Registro FrontEnd v2

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from "./register.module.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
@@ -5,19 +6,88 @@ import Book from "../../assets/images/book.png";
 import Background from "../../assets/images/Background.png";
 
 const Register = () => {
+
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+      });
+    
+      const [formErrors, setFormErrors] = useState({});    
+    
+      const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+          ...formData,
+          [name]: value,
+        });
+      
+        // Eliminar el mensaje de error asociado a este campo
+        setFormErrors({
+          ...formErrors,
+          [name]: '', // Resetear el error si se está escribiendo en el campo
+        });
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        const errors = {};
+    
+        // Validaciones
+        if (formData.password !== formData.confirmPassword) {
+          errors.confirmPassword = 'Las contraseñas no coinciden';
+        }
+        if (formData.password.length < 8) {
+          errors.password = 'La contraseña debe tener al menos 8 caracteres';
+        }
+        // Validación del email (expresiones regulares)
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(formData.email)) {
+          errors.email = 'El email no es válido';
+        }
+    
+        // Validación de campos requeridos (por ejemplo, nombre y apellido)
+        if (!formData.firstName.trim()) {
+          errors.firstName = 'Por favor, ingresa tu nombre';
+        }
+        if (!formData.lastName.trim()) {
+          errors.lastName = 'Por favor, ingresa tu apellido';
+        }
+
+        // Otras validaciones necesarias...
+    
+        if (Object.keys(errors).length === 0) {
+          // Envío del formulario si no hay errores
+          // Aquí se implementa una función para enviar los datos
+        } else {
+          setFormErrors(errors);
+        }
+      };
     return <div className={styles.registerContainer}>
         <div className={styles.formContainer} style={{ backgroundImage: `url(${Background})` }}>            
             <h1 className={styles.signUp}>Sign Up</h1>
             <h2 className={styles.createYourAccount}>Create Your Account</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className={styles.fullnameRow}>
                     <div className={styles.labelinput}>
                         <label htmlFor="firstName">Name</label>
-                        <input type="text" id="firstName" placeholder="Ingresa tu nombre" />
+                        <input type="text" id="firstName" name='firstName' value={formData.firstName} onChange={handleInputChange} placeholder="Ingresa tu nombre"/>
+                        {formErrors.firstName && <p className="error" style={{ color: 'red', fontSize: '12px',textAlign: 'Start'}}>{formErrors.firstName}</p>}
                     </div>
                     <div className={styles.labelinput}>
                         <label htmlFor="lastName">Last Name</label>
-                        <input type="text" id="lastName" placeholder="Ingresa tu apellido" />
+                        <input 
+                        type="text" 
+                        id="lastName"
+                        name='lastName' 
+                        value={formData.lastName}
+                        onChange={handleInputChange}
+                        placeholder="Ingresa tu apellido" 
+                        />
+                        {formErrors.lastName && <p className="error" style={{ color: 'red', fontSize: '12px',textAlign: 'Start' }}>{formErrors.lastName}</p>}
                     </div>
                 </div>
                 <div className={styles.formRow}>
@@ -29,19 +99,40 @@ const Register = () => {
                 <div className={styles.formRow}>
                     <div className={styles.labelinput}>
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" placeholder="Ingresa tu correo electrónico" />
+                        <input 
+                        type="email" 
+                        id="email"
+                        name='email'
+                        value={formData.email}
+                        onChange={handleInputChange}
+                        placeholder="Ingresa tu correo electrónico" />
+                        {formErrors.email && <p className="error" style={{ color: 'red', fontSize: '12px', textAlign: 'Start' }}>{formErrors.email}</p>}
                     </div>
                 </div>
                 <div className={styles.formRow}>
                     <div className={styles.labelinput}>
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" placeholder="Ingresa tu contraseña" />
+                        <input 
+                        type="password" 
+                        id="password" 
+                        name='password'
+                        value={formData.password}
+                        onChange={handleInputChange}
+                        placeholder="Ingresa tu contraseña" />
+                        {formErrors.password && <p className="error" style={{ color: 'red', fontSize: '12px',textAlign: 'Start' }}>{formErrors.password}</p>}
                     </div>
                 </div>
                 <div className={styles.formRow}>
                     <div className={styles.labelinput}>
                         <label htmlFor="confirmPassword">Confirm Password</label>
-                        <input type="password" id="confirmPassword" placeholder="Confirma tu contraseña" />
+                        <input 
+                        type="password" 
+                        id="confirmPassword"
+                        name='confirmPassword'
+                        value={formData.confirmPassword}
+                        onChange={handleInputChange}
+                        placeholder="Confirma tu contraseña" />
+                        {formErrors.confirmPassword && <p className="error" style={{ color: 'red', fontSize: '12px', textAlign: 'Start' }}>{formErrors.confirmPassword}</p>}
                     </div>
                 </div>
                 <p><a href="#" style={{ fontSize: '20px' }}>Forget your password?</a></p>
