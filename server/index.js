3cec0b2fe13d55285e97d3643c1766029784bd70
FrankR27 <frankdejesusramosguzman9@gmail.com>
2023-11-24 18:24:42 -0400

    index-repair

--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const connection = require("./context/connection");
 const dotenv = require("dotenv");
+const authRoutes = require("./router/login");
+const cors = require("cors");
 
-//ROUTES
-const signupRouter = require("./router/signup");
-//CONFIG
 const app = express();
 dotenv.config();
 
-app.use()
-app.listen(process.env.PORT, () => {});
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors())
+
+app.get("/", (req, res) => {
+   res.json({ working: "THE APP IS WORKING" });
+});
+
+app.use("/api/", authRoutes);
+
+app.listen(process.env.PORT, () => {
+   console.log("server started on port 5000");
+});
 
 module.exports = app;
