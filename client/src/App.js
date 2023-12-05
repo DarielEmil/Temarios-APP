ba187e76fb6b4abdeabcef9bbad59e10f6412918
Restituy0 <dariel_eduardo@hotmail.com>
2023-12-04 22:50:37 -0400

    Landing Page v1

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import { Route } from "react-router-dom/cjs/react-router-dom";
 import "./App.css";
 import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
 import { BrowserRouter as Router, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import Landing from "./components/LandingPage/Landing";
 
 function App() {
    return (
@@ -10,6 +12,12 @@ function App() {
             <Switch>
                <Route path="/signup">
                   <Register />
+               </Route>
+               <Route path="/login">
+                  <Login />
+               </Route>
+               <Route path="/">
+                  <Landing />
                </Route>
             </Switch>
          </div>
