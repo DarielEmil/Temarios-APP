37abf4ca8bfd54fbabb34d2a87c384c8e6cd9b3d
Wander2 <wanderssff1@gmail.com>
2023-12-05 02:00:26 -0400

    version

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,30 @@
-// App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
-import Login from './components/Login/Login';
-//import Landing from './components/Landingpage/Landing'; 
+
+import { Route } from "react-router-dom/cjs/react-router-dom";
+import "./App.css";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+import { BrowserRouter as Router, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import Landing from "./components/LandingPage/Landing";
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/login" component={Login} />
-        </Switch>
-      </div>
-    </Router>
-  );
+   return (
+      <Router>
+         <div className="App">
+            <Switch>
+               <Route path="/signup">
+                  <Register />
+               </Route>
+               <Route path="/login">
+                  <Login />
+               </Route>
+               <Route path="/">
+                  <Landing />
+               </Route>
+            </Switch>
+         </div>
+      </Router>
+   );
+
 }
 
 export default App;
