17136d8f9d476bcef549163143c52420ea6a7733
Cristian Eulises Sanchez Ramirez <scristianeulises@gmail.com>
2023-12-15 17:41:56 -0400

    fix merge

--- server/router/create-plan.js
+++ server/router/create-plan.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const planController = require("../controllers/planController");
+const { createPlan, getPlan, getPlanContent, getParrafos } = require("../controllers/planController");
 
-router.get("/create-plan", planController.createPlan);
+router.post("/create-plan", createPlan);
+router.get("/get-plan/:user", getPlan);
+router.post("/get-plan-content", getPlanContent);
+router.post("/get-parrafos", getParrafos);
 
 module.exports = router;
--- server/util/planCreationOptions.js
+++ server/util/planCreationOptions.js
@@ -13,19 +13,22 @@ const createPrompt = (questions, language) => {
         SOPORTARA STRINGS HASTA DE MIL PALABRAS.
         DA INFORMACION NO DE QUE TRATARAN LOS TEMAS, NO USES SALTOS DE LINEA
         los párrafos deben de explicar las cosas, LOS PARRAFOS NO DEBEN DE DECIR DE QUE VA A TRATAR LA SEMANA
+        DEVOLVER UN JSON BIEN HECHO
         El plan de estudio tiene que ser extenso, y lo vas a dividir en la siguiente estructura el json:
+
+
         
-        plan_de_estudios
-               semana (number)
-               tema (str)
-               contenido (array de parrafos: [{"asdfasdfasfd"}, {"asdfasdfasdf}]) //CADA PARRAFO DEBE DE TENER MAS DE 500 PALABRAS
-               ejercicios (array de strings indicando el mandato del ejercicio)
+        titulo: (string)
+        descripcion: (string)
+        plan_de_estudios: (array)[
+           tema (str)
+           contenido (array de parrafos: [{"asdfasdfasfd"}, {"asdfasdfasdf}]) //CADA PARRAFO DEBE DE TENER MAS DE 500 PALABRAS
+           ejercicios (array de strings indicando el mandato del ejercicio)
+           semana (number)
+           recurso: paginas y recursos adicionales
+         ]
 
-        recurso: paginas y recursos adicionales
-        
-  
-
-
+         RESPETA EL ORDEN DEL JSON, HAZLO OBLIGATORIAMENTE EN ESE ORDEN Y CON ESOS TIPOS
     `;
    return prompt;
 };
@@ -34,4 +37,14 @@ const createJSON = (text) => {
    return JSON.parse(text.choices[0].message.content);
 };
 
-module.exports = { createPrompt, createJSON };
+const createPromptParrafo = (text) => {
+   return `
+   Generar tres parrafos sobre estos temas: ${text}
+   Cada parrafo debe de tener mas de 500 palabras,
+   Solo desarrolla el contenido de la semana que esta en curso
+   devolverlo en un archivo json de la siguiente manera:
+
+   parrafos: ["text+500palabras", "texto+500palabras", "texto+500palabras",]
+   `
+};
+module.exports = { createPrompt, createJSON,createPromptParrafo };
